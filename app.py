7970572bbe992a6d39fc017885400e660fc2b5ec
Sonal Ranjit <sonal.ranjit3@gmail.com>
2018-02-21 21:55:18 -0500

    updating procfile and app

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def welcome_message():
     return message
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
